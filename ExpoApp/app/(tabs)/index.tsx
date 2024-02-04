8c255796a69bfeebe9ef8a883e5b087c294fc817
jonathanoh59 <81584200+jonathanoh59@users.noreply.github.com>
2024-02-04 17:50:08 +0000

    hello world Text JSX

--- ExpoApp/app/(tabs)/index.tsx
+++ ExpoApp/app/(tabs)/index.tsx
@@ -6,6 +6,7 @@ import { Text, View } from '@/components/Themed';
 export default function TabOneScreen() {
   return (
     <View style={styles.container}>
+      <Text>Hello World!</Text>
       <Text style={styles.title}>Tab One</Text>
       <View style={styles.separator} lightColor="#eee" darkColor="rgba(255,255,255,0.1)" />
       <EditScreenInfo path="app/(tabs)/index.tsx" />
